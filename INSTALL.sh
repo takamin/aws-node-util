acb25aa81c566c67b6b19bbfc1e6c97f5297a988
Koji Takami <vzg03566@gmail.com>
2016-02-29 13:55:20 +0900

    Add node modules to call AWSCLI

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,8 +3,11 @@ NODE_MODULE=~/node_modules
 install aws-lambda-get.sh ${DSTBIN}/aws-lambda-get
 install aws-lambda-create.sh ${DSTBIN}/aws-lambda-create
 install aws-lambda-upload.sh ${DSTBIN}/aws-lambda-upload
+install aws-apigw-describe-all.js ${DSTBIN}/aws-apigw-describe-all
 install aws-dynamodb-tbldesc2create.sh ${DSTBIN}/aws-dynamodb-tbldesc2create
 install aws-dynamodb-create-table.sh ${DSTBIN}/aws-dynamodb-create-table
 install aws-dynamodb-delete-table.sh ${DSTBIN}/aws-dynamodb-delete-table
 install node_modules/aws-lambda.js ${NODE_MODULE}
 install node_modules/aws-dynamodb.js ${NODE_MODULE}
+install node_modules/awscli.js ${NODE_MODULE}
+install node_modules/aws-apigateway.js ${NODE_MODULE}
