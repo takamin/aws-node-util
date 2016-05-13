b000e6d3ec87a395ca7f8ed0e9aadc7ffb7b6905
Koji Takami <vzg03566@gmail.com>
2016-05-13 16:54:45 +0900

    Add some dynamodb commands

---
+++ bin/aws-dynamodb-list-tables.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+(function() {
+    "use strict";
+    var aws = require('../lib/awscli');
+    var listit = require('list-it');
+    aws.dynamodb.listTables(function(err, data) {
+        if(err) {
+            console.error("Error:", err);
+            process.exit(1);
+        }
+        var buf = listit.buffer();
+        buf.d(["#", "tableName"]);
+        var i = 0;
+        data.TableNames.forEach(function(tableName) {
+            buf.d([++i, tableName]);
+        });
+        console.log(buf.toString());
+    });
+}());
