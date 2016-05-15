e8028b956852154eb2e3b7be64888ddaf7aedb56
Koji Takami <vzg03566@gmail.com>
2016-05-15 16:22:42 +0900

    Fix aws-dynamodb-desc2create

--- bin/aws-dynamodb-tbldesc2create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ "$1" = "" ]; then
-    echo "usage: aws-dynamodb-tbldesc2create <table-name>"
-    exit
-fi
-aws dynamodb describe-table --table-name $1 > $1.json
-node -e "require('aws-dynamodb').putCreateTableJson('$1.json')"
-rm $1.json
