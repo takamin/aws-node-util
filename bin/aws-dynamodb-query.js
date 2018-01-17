eb45f26e0b1dce897ab850efad583927f5b88a21
Koji Takami <vzg03566@gmail.com>
2018-01-17 22:22:40 +0900

    Fix the expression parser
    When both of 'filter-expression' and 'key-condition-expression' are
    specified, the internal indexies of value's placeholder had conflicted.
    
    A 'dynamodb-statement' module is added.

--- bin/aws-dynamodb-query.js
+++ bin/aws-dynamodb-query.js
@@ -1,10 +1,8 @@
 #!/bin/env node
 (function() {
     "use strict";
-    var aws = require('../lib/awscli');
-    var DynamoDB = aws.getService("DynamoDB");
-    //aws.setDebug();
     var dynamodb = require('../lib/aws-dynamodb');
+    var Statement = require("../lib/dynamodb-statement");
     var getopt = require('node-getopt').create([
         ['c', 'max-items=ARG',          'The total number of items to return'],
         ['n', 'starting-token=ARG',     'A token to specify where to start paginating'],
@@ -41,121 +39,60 @@
     var sortItemPath = getopt.options['sort-item'];
     var sortDesc = getopt.options['desc'];
 
-    var scanOpts = {};
-    scanOpts["TableName"] = arg.tableName;
-    scanOpts["Limit"] = maxItems;
+    var statement = new Statement();
+    statement.setTableName(arg.tableName);
+    statement.setLimit(maxItems);
 
-    //
-    // Extra options
-    //
-    var extraOptions = {};
-    var projectionExpression = [];
-    var filterExpression = "";
-    var expressionAttributeNames = {};
-    var expressionAttributeValues = {};
-    var select = "ALL_ATTRIBUTES";
-
-    //
     // projection expression
-    //
     var projexpr = getopt.options["projection-expression"];
     if(projexpr) {
-        try {
-            scanOpts["ProjectionExpression"] =
-                dynamodb.parseProjectionExpression(
-                        projexpr, expressionAttributeNames);
-            select = "ALL_PROJECTED_ATTRIBUTES";
-        } catch (err) {
-            console.error("Error in projection-expression:", err.message);
-            process.exit(1);
-        }
+        statement.setProjectionExpression(projexpr);
     }
 
-    //
     // Query expression
-    //
-    (function() {
-        var keyConditionExpr = null;
-        var argExpr = arg.keyConditionExpression;
-        var optExpr = getopt.options["key-condition-expression"];
-        if(argExpr) {
-           if(optExpr) {
-               console.error("Error:",
-                       "Key condition expression is specified",
-                       "at both command line and option.");
-               process.exit(1);
-           }
-           keyConditionExpr = argExpr;
-        } else if(optExpr) {
-           keyConditionExpr = optExpr;
-        } else {
-           console.error("Error:",
-                   "The key condition expression is required.");
-           process.exit(1);
-        }
-        try {
-            scanOpts["KeyConditionExpression"] =
-                dynamodb.parseConditionExpression(keyConditionExpr,
-                    expressionAttributeNames, expressionAttributeValues);
-        } catch (err) {
-            console.error("Error in key-condition-expression:", err.message);
-            process.exit(1);
-        }
-    }());
+    var keyConditionExpr = null;
+    var argExpr = arg.keyConditionExpression;
+    var optExpr = getopt.options["key-condition-expression"];
+    if(argExpr && optExpr) {
+       console.error("Error:",
+               "Key condition expression is specified",
+               "at both command line and option.");
+       process.exit(1);
+    } else if(!argExpr && !optExpr) {
+       console.error("Error:",
+               "The key condition expression is required.");
+       process.exit(1);
+    } else if(argExpr) {
+       keyConditionExpr = argExpr;
+    } else if(optExpr) {
+       keyConditionExpr = optExpr;
+    }
+    statement.setKeyConditionExpression(keyConditionExpr);
 
-    //
     // Filter expression
-    //
     if(getopt.options["filter-expression"]) {
-        try {
-            scanOpts["FilterExpression"] =
-                dynamodb.parseConditionExpression(
-                    getopt.options["filter-expression"],
-                    expressionAttributeNames,
-                    expressionAttributeValues);
-        } catch (err) {
-            console.error("Error in filter-expression:", err.message);
-            process.exit(1);
-        }
+        statement.setFilterExpression(getopt.options["filter-expression"]);
     }
 
-    //
-    // Expression attribute names
-    //
-    if(Object.keys(expressionAttributeNames).length > 0) {
-        scanOpts["ExpressionAttributeNames"] =
-            expressionAttributeNames;
-    }
-
-    //
-    // Expression attribute values
-    //
-    if(Object.keys(expressionAttributeValues).length > 0) {
-        scanOpts["ExpressionAttributeValues"] =
-            expressionAttributeValues;
-    }
-
-    //
     // Dry-run Option
-    //
     if(getopt.options["dry-run"]) {
+        var parameter = statement.getQueryParameter();
         console.log("// opts for aws.dynamodb.query:");
-        console.log(JSON.stringify(scanOpts, null, "    "));
-        process.exit(0);
+        console.log(JSON.stringify(parameter, null, "    "));
+    } else {
+        statement.queryAll(function(err, data) {
+            if(err) {
+                console.error("Error:", err);
+                process.exit(1);
+            }
+            if(getopt.options['output-json']) {
+                console.log(JSON.stringify(data, null, "    "));
+            } else if(getopt.options['output-json-oneline']) {
+                console.log(JSON.stringify(data));
+            } else {
+                dynamodb.printScanResult(data, sortItemPath, sortDesc);
+            }
+        });
     }
-
-    DynamoDB.query(scanOpts, function(err, data) {
-        if(err) {
-            console.error("Error:", err);
-            process.exit(1);
-        }
-        if(getopt.options['output-json']) {
-            console.log(JSON.stringify(data, null, "    "));
-        } else if(getopt.options['output-json-oneline']) {
-            console.log(JSON.stringify(data));
-        } else {
-            dynamodb.printScanResult(data, sortItemPath, sortDesc);
-        }
-    });
 }());
 
